63a0c882949b289cdef9fa3211549b481c194e74
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-11-28 15:02:30 +0100

    Add control_flow_function

---
+++ src/manim_dataflow_analysis/control_flow_function.py
@@ -0,0 +1,19 @@
+from manim_dataflow_analysis.abstract_environment import AbstractEnvironment
+from manim_dataflow_analysis.cfg import ProgramPoint
+from manim.mobject.text.tex_mobject import MathTex
+from typing import TypeVar, Protocol
+from abc import abstractmethod
+
+L = TypeVar("L")
+
+
+class ControlFlowFunction(Protocol[L]):
+    @abstractmethod
+    def condition_tex(self) -> MathTex: ...
+
+    @abstractmethod
+    def apply(
+        self,
+        abstract_environment: AbstractEnvironment,
+        program_point: ProgramPoint,
+    ) -> AbstractEnvironment | None: ...
