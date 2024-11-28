39ccf3962de35fb5d465a0970fff9138f3ea99c8
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-11-28 17:53:47 +0100

    Use a protocol for ControlFlowFunctions

--- src/manim_dataflow_analysis/control_flow_function.py
+++ src/manim_dataflow_analysis/control_flow_function.py
@@ -3,44 +3,21 @@ from manim_dataflow_analysis.cfg import ProgramPoint
 from manim.mobject.text.tex_mobject import MathTex
 from manim.mobject.mobject import Group
 from typing import TypeVar, Generic, Protocol
-from dataclasses import dataclass
 from abc import abstractmethod
 
 L = TypeVar("L")
 
 
-class ControlFlowFunctionApplyFunction(Protocol[L]):
+class ControlFlowFunctions(Protocol[L]):
     @abstractmethod
-    def __call__(
-        self, abstract_environment: AbstractEnvironment[L], program_point: ProgramPoint
-    ) -> AbstractEnvironment[L] | None: ...
+    def functions(self) -> tuple[tuple[str, str], ...]: ...
 
-
-@dataclass(frozen=True)
-class ControlFlowFunction(Generic[L]):
-    condition_tex: str
-    modification_tex: str
-    apply_function: ControlFlowFunctionApplyFunction[L]
-
-
-@dataclass(init=False, frozen=True)
-class ControlFlowFunctions(Generic[L]):
-    functions: tuple[ControlFlowFunction[L], ...]
-
-    def __init__(self, *functions: ControlFlowFunction[L]):
-        object.__setattr__(self, "functions", functions)
-
+    @abstractmethod
     def apply(
-        self, abstract_environment: AbstractEnvironment[L], program_point: ProgramPoint
-    ) -> tuple[AbstractEnvironment[L], ControlFlowFunction[L]] | None:
-        for function in self.functions:
-            new_abstract_environment = function.apply_function(
-                abstract_environment, program_point
-            )
-            if new_abstract_environment is not None:
-                return new_abstract_environment, function
-
-        return None
+        self,
+        abstract_environment: AbstractEnvironment[L],
+        program_point: ProgramPoint,
+    ) -> tuple[AbstractEnvironment[L], int] | None: ...
 
 
 LINE_LENGTH = 9
@@ -53,30 +30,25 @@ class ControlFlowFunctionsTex(MathTex, Generic[L]):
     def __init__(self, control_flow_functions: ControlFlowFunctions[L]):
         tex_strings: list[str] = []
 
-        for function in control_flow_functions.functions:
+        for modification_tex, condition_tex in control_flow_functions.functions():
             tex_strings.extend(
                 (
                     "&",
                     r"fg[[p]](\phi) = &" if not tex_strings else "&",
-                    function.modification_tex,
+                    modification_tex,
                     r"& \quad",
-                    "if" if function.condition_tex else "otherwise",
+                    "if" if condition_tex else "otherwise",
                     "&",
-                    function.condition_tex,
+                    condition_tex,
                     "&",
                     r"\\",
                 )
             )
 
         super().__init__(*tex_strings)
-        self.control_flow_functions = control_flow_functions
 
-    def get_condition_part(self, function: ControlFlowFunction[L]) -> Group:
-        i = self.control_flow_functions.functions.index(function)
+    def get_condition_part(self, index: int) -> Group:
+        return self[index * LINE_LENGTH + CONDITION_PART_INDEX]
 
-        return self[i * LINE_LENGTH + CONDITION_PART_INDEX]
-
-    def get_modification_part(self, function: ControlFlowFunction[L]) -> Group:
-        i = self.control_flow_functions.functions.index(function)
-
-        return self[i * LINE_LENGTH + MODIFICATION_PART_INDEX]
+    def get_modification_part(self, index: int) -> Group:
+        return self[index * LINE_LENGTH + MODIFICATION_PART_INDEX]
