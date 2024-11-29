db98aebba44a8bc4fd92da302376fb74a46eaab8
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-11-29 23:54:38 +0100

    Improve AbstractEnvironment

--- src/manim_dataflow_analysis/abstract_environment.py
+++ src/manim_dataflow_analysis/abstract_environment.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from typing import TypeVar, Generic, Iterable
+from typing import TypeVar, Generic, Iterable, Generator
 from manim.mobject.types.vectorized_mobject import VGroup, VMobject
-from manim.mobject.text.tex_mobject import MathTex
+from manim.mobject.text.tex_mobject import MathTex, SingleStringMathTex
 from manim_dataflow_analysis.lattice import Lattice
 from dataclasses import dataclass
 from frozendict import frozendict
@@ -17,33 +17,42 @@ class AbstractEnvironment(Generic[L]):
     lattice: Lattice[L]
     variables: frozendict[str, L] = frozendict()
 
+    def join_generator(
+        self, other: AbstractEnvironment[L]
+    ) -> Generator[tuple[str, L], None, None]:
+        for variable in self.variables | other.variables:
+            self_abstract_value = self.variables.get(variable)
+            other_abstract_value = self.variables.get(variable)
+
+            if self_abstract_value is None:
+                yield variable, other_abstract_value
+            elif other_abstract_value is None:
+                yield variable, self_abstract_value
+            else:
+                yield variable, self.lattice.join(
+                    self_abstract_value, other_abstract_value
+                )
+
     def join(self, other: AbstractEnvironment[L]) -> AbstractEnvironment[L]:
         return AbstractEnvironment(
             self.lattice,
-            frozendict(
-                (
-                    variable,
-                    self.lattice.join(
-                        self.variables.get(variable, self.lattice.bottom()),
-                        other.variables.get(variable, self.lattice.bottom()),
-                    ),
-                )
-                for variable in self.variables | other.variables
-            ),
+            frozendict(self.join_generator(other)),
         )
 
-    def includes(self, other: AbstractEnvironment[L]) -> bool:
+    def includes_generator(
+        self, other: AbstractEnvironment[L]
+    ) -> Generator[tuple[str, bool], None, None]:
         for variable in self.variables | other.variables:
             self_abstract_value = self.variables.get(variable)
-            other_abstract_value = other.variables.get(variable)
+            other_abstract_value = self.variables.get(variable)
 
-            if self_abstract_value is None or (
+            yield variable, self_abstract_value is None or (
                 other_abstract_value is not None
-                and not self.lattice.includes(self_abstract_value, other_abstract_value)
-            ):
-                return False
+                and self.lattice.includes(self_abstract_value, other_abstract_value)
+            )
 
-        return True
+    def includes(self, other: AbstractEnvironment[L]) -> bool:
+        return all(included for _, included in self.includes_generator(other))
 
     def set(self, **variables: L) -> AbstractEnvironment[L]:
         return AbstractEnvironment(self.lattice, self.variables | variables)
@@ -90,6 +99,8 @@ class AbstractEnvironmentUpdateRules(MathTex, Generic[L]):
     def get_condition_part(self, index: int) -> VMobject:
         if_otherwise = self.submobjects[index * LINE_LENGTH + IF_OTHERWISE_INDEX]
         condition_part = self.submobjects[index * LINE_LENGTH + CONDITION_PART_INDEX]
+
+        assert isinstance(condition_part, SingleStringMathTex)
 
         if condition_part.get_tex_string() == EMPTY_CHARACTER:
             return if_otherwise
